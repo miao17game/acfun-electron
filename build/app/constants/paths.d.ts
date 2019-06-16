c17ba979482c140548f77afe27c0d8f950393e40
Big Mogician <245396954@qq.com>
2019-06-16 14:29:20 +0800

    fix: ignore build folder

--- build/app/constants/paths.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const OS_HOME: string;
-export declare const ROOT_FOLDER: string;
-export declare const PREFERENCE_CONF: string;
--- build/app/constants/paths.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var os = require("os");
-exports.OS_HOME = os.homedir();
-exports.ROOT_FOLDER = os.homedir() + "/Documents/xiaokuaiji";
-exports.PREFERENCE_CONF = exports.ROOT_FOLDER + "/.preference.conf";
-//# sourceMappingURL=paths.js.map
